39ad14ddf865f1ff4e7d03062d68ead452efb78e
Gecolay <Discord.Benjamin@web.de>
2022-06-07 22:26:58 +0200

    Change utils

--- v1_17_R1/src/main/java/dev/geco/gsit/mcv/v1_17_R1/util/SpawnUtil.java
+++ v1_17_R1/src/main/java/dev/geco/gsit/mcv/v1_17_R1/util/SpawnUtil.java
@@ -10,22 +10,13 @@ import dev.geco.gsit.mcv.v1_17_R1.objects.*;
 
 public class SpawnUtil implements ISpawnUtil {
 
+    private final dev.geco.gsit.util.SpawnUtil spawnutil = new dev.geco.gsit.util.SpawnUtil();
+
     public boolean needCheck() { return false; }
 
     public boolean checkLocation(Location Location) { return true; }
 
-    public boolean checkPlayerLocation(Entity Holder) {
-
-        if(!needCheck()) return true;
-
-        Entity e = createPlayerSeatEntity(Holder, null);
-
-        boolean v = e.isValid();
-
-        e.remove();
-
-        return v;
-    }
+    public boolean checkPlayerLocation(Entity Holder) { return spawnutil.checkPlayerLocation(Holder); }
 
     public Entity createSeatEntity(Location Location, Entity Rider) {
 
@@ -38,20 +29,6 @@ public class SpawnUtil implements ISpawnUtil {
         return sas.getBukkitEntity();
     }
 
-    public Entity createPlayerSeatEntity(Entity Holder, Entity Rider) {
-
-        return Holder.getWorld().spawn(Holder.getLocation(), AreaEffectCloud.class, b -> {
-            try { b.setRadius(0); } catch(Exception ignored) { }
-            try { b.setGravity(false); } catch(Exception ignored) { }
-            try { b.setInvulnerable(true); } catch(Exception ignored) { }
-            try { b.setDuration(Integer.MAX_VALUE); } catch(Exception ignored) { }
-            try { b.setParticle(Particle.BLOCK_CRACK, Material.AIR.createBlockData()); } catch(Exception ignored) { }
-            try { b.setWaitTime(0); } catch(Exception ignored) { }
-            if(Rider != null && Rider.isValid()) {
-                Holder.addPassenger(b);
-                b.addPassenger(Rider);
-            }
-        });
-    }
+    public Entity createPlayerSeatEntity(Entity Holder, Entity Rider) { return spawnutil.createPlayerSeatEntity(Holder, Rider); }
 
 }
