406ecced0e8aa5612d77116965b339ba69c70ac1
Gecolay <Discord.Benjamin@web.de>
2022-09-26 17:49:27 +0200

    Update SpawnUtil for paper-chunk-changes

--- v1_19_R1_2/src/main/java/dev/geco/gsit/mcv/v1_19_R1_2/util/SpawnUtil.java
+++ v1_19_R1_2/src/main/java/dev/geco/gsit/mcv/v1_19_R1_2/util/SpawnUtil.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.*;
 import org.bukkit.craftbukkit.v1_19_R1.*;
 import org.bukkit.craftbukkit.v1_19_R1.entity.*;
 
+import net.minecraft.world.level.entity.*;
+
 import dev.geco.gsit.util.*;
 import dev.geco.gsit.mcv.v1_19_R1_2.objects.*;
 
@@ -22,7 +24,7 @@ public class SpawnUtil implements ISpawnUtil {
 
         if(Rider != null && Rider.isValid()) ((CraftEntity) Rider).getHandle().startRiding(seatEntity, true);
 
-        ((CraftWorld) Location.getWorld()).getHandle().entityManager.addNewEntity(seatEntity);
+        spawnEntity(Location.getWorld(), seatEntity);
 
         if(Rotate) seatEntity.startRotate();
 
@@ -40,9 +42,24 @@ public class SpawnUtil implements ISpawnUtil {
             ((CraftEntity) Rider).getHandle().startRiding(playerSeatEntity, true);
         }
 
-        ((CraftWorld) Holder.getWorld()).getHandle().entityManager.addNewEntity(playerSeatEntity);
+        spawnEntity(Holder.getWorld(), playerSeatEntity);
 
         return playerSeatEntity.getBukkitEntity();
+    }
+
+    private void spawnEntity(World Level, net.minecraft.world.entity.Entity Entity) {
+
+        try {
+
+            ((CraftWorld) Level).getHandle().entityManager.addNewEntity(Entity);
+        } catch (Throwable paper) {
+
+            try {
+
+                LevelEntityGetter<net.minecraft.world.entity.Entity> levelEntityGetter = ((CraftWorld) Level).getHandle().getEntities();
+                levelEntityGetter.getClass().getMethod("addNewEntity", net.minecraft.world.entity.Entity.class).invoke(levelEntityGetter, Entity);
+            } catch (Throwable e) { e.printStackTrace(); }
+        }
     }
 
 }
