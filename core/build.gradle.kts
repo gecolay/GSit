eece315045d6af466c7b4a7cac5681d5ffa1c358
Gecolay <Discord.Benjamin@web.de>
2024-12-23 19:00:01 +0100

    Update resolutionStrategy

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,6 +10,15 @@ dependencies {
     compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.4.0")
 }
 
+configurations.all {
+    resolutionStrategy {
+        // worldguard-bukkit & plotsquared-core require outdated versions compared to paper-api, so we force the new version
+        force("com.google.guava:guava:33.3.1-jre")
+        force("com.google.code.gson:gson:2.11.0")
+        force("it.unimi.dsi:fastutil:8.5.15")
+    }
+}
+
 java {
     disableAutoTargetJvm()
 }
