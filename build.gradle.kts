24eed3d5ec5bcfc43bd5ef56fd409276a06570c1
Gecolay <Discord.Benjamin@web.de>
2025-04-05 23:46:39 +0200

    Release 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("com.gradleup.shadow") version "9.0.0-beta11"
+    id("com.gradleup.shadow") version "9.0.0-beta12"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" apply false
 }
 
