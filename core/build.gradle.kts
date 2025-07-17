c63b26a1c9a607435983fea4fb41135a3c333bbf
Gecolay <Discord.Benjamin@web.de>
2025-07-17 18:46:41 +0200

    Add 1.21.8 support

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.7-R0.1-SNAPSHOT")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13") {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.14") {
         exclude("com.google.guava", "guava")
         exclude("com.google.code.gson", "gson")
         exclude("it.unimi.dsi", "fastutil")
     }
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.github.GriefPrevention:GriefPrevention:16.18.4")
-    compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.5.3")
+    compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.5.4")
 }
 
 java {
--- mcv/v1_21_6/build.gradle.kts
+++ mcv/v1_21_6/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     compileOnly(project(":core"))
-    paperweight.paperDevBundle("1.21.7-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
