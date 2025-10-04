e5d15873db0ec7560f1d8f3672393d6f9946ffb8
Gecolay <Discord.Benjamin@web.de>
2025-10-04 21:24:25 +0200

    Try to fix #263

--- core/src/main/java/dev/geco/gsit/service/PlayerSitService.java
+++ core/src/main/java/dev/geco/gsit/service/PlayerSitService.java
@@ -88,7 +88,8 @@ public class PlayerSitService {
             for(UUID passenger : passengers.getValue()) {
                 Entity passengerEntity = Bukkit.getEntity(passenger);
                 if(passengerEntity == null) continue;
-                passengerEntity.remove();
+                if(gSitMain.isFoliaServer()) gSitMain.getTaskService().run(passengerEntity::remove, passengerEntity);
+                else passengerEntity.remove();
             }
             String key = source.getUniqueId().toString() + passengers.getKey();
             Long spawnTime = spawnTimes.get(key);
@@ -104,7 +105,8 @@ public class PlayerSitService {
             for(UUID vehicle : vehicles.getValue()) {
                 Entity vehicleEntity = Bukkit.getEntity(vehicle);
                 if(vehicleEntity == null) continue;
-                vehicleEntity.remove();
+                if(gSitMain.isFoliaServer()) gSitMain.getTaskService().run(vehicleEntity::remove, vehicleEntity);
+                else vehicleEntity.remove();
             }
             String key = vehicles.getKey().toString() + source.getUniqueId();
             Long spawnTime = spawnTimes.get(key);
