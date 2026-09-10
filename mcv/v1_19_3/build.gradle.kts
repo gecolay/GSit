3ac9e874c52132dc50375e3cf3bff0128b355c11
Gecolay <Discord.Benjamin@web.de>
2026-09-10 23:36:48 +0200

    Fix gradle build

--- mcv/v1_19_3/build.gradle.kts
+++ mcv/v1_19_3/build.gradle.kts
@@ -10,9 +10,6 @@ dependencies {
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
 
-paperweight.javaLauncher = javaToolchains.launcherFor {
-    languageVersion = JavaLanguageVersion.of(21)
-}
 
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(20)
