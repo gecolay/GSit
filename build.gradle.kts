5c8aec2c10d35c8f4e5eea3171d88517d939ad54
Gecolay <Discord.Benjamin@web.de>
2026-09-15 17:54:50 +0200

    Add v26_3 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     api(project(":v1_21_11", io.papermc.paperweight.util.constants.REOBF_CONFIG))
     api(project(":v26_1", "default"))
     api(project(":v26_2", "default"))
+    api(project(":v26_3", "default"))
 }
 
 tasks {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.3.build.+")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.18") {
         exclude("com.google.guava", "guava")
         exclude("com.google.code.gson", "gson")
